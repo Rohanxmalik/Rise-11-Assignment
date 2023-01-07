03846055c72154376ee81ab8fc305b73e7f886be
Rohanxmalik <rohanxmalik06@gmail.com>
2023-01-07 23:58:10 +0530

    first commit

---
+++ src/city-list.js
@@ -0,0 +1,60 @@
+export default {
+    status : "success",
+    data : [
+        {
+            id : 1,
+            title : "Las Vegas",
+            code : "NV (LAS)"
+        },
+        {
+            id : 2,
+            title : "Los Angeles",
+            code : "CA (LAX)"
+        },
+        {
+            id : 3,
+            title : "New York City",
+            code : "NY (NYC)"
+        },
+        {
+            id : 4,
+            title : "Palm Springs",
+            code : "CA (PSP)"
+        },
+        {
+            id : 5,
+            title : "Portland",
+            code : "OR (PDX)"
+        },
+        {
+            id : 6,
+            title : "San Francisco",
+            code : "CA (SF)"
+        },
+        {
+            id : 7,
+            title : "Beverly Hills",
+            code : "CA (BH)"
+        },
+        {
+            id : 8,
+            title : "San Jones",
+            code : "CA (SJ)"
+        },
+        {
+            id : 9,
+            title : "Washington, D.C.",
+            code : "USA (DC)"
+        },
+        {
+            id : 10,
+            title : "Chicago",
+            code : "IL (C)"
+        },
+        {
+            id : 11,
+            title : "Michigan",
+            code : "NV (MI)"
+        }
+    ]
+}
